52c14b1539161464c7d454f699ec327a696b52ce
Kevin Owens <kowens1@gmail.com>
2022-07-01 10:56:57 -0600

    Update osx bootstrap

--- bin/osx_bootstrap.sh
+++ bin/osx_bootstrap.sh
@@ -29,6 +29,7 @@ brew update
 
 PACKAGES=(
     ack
+    asdf
     git
     libyaml
     libffi
@@ -62,8 +63,7 @@ cd fonts
 cd ..
 rm -rf fonts
 
-#install asdf
-git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.5.1
+#install asdf plugins
 
 asdf plugin-add ruby
 asdf plugin-add nodejs
